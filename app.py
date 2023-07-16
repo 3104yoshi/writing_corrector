bd3c111544ee02fd195b533f030e64fda5a82204
3104yoshi <3104adgjmp@gmail.com>
2023-07-16 23:34:04 +0900

    add function comparing translation by deepl with one by self

--- app.py
+++ app.py
@@ -27,36 +27,20 @@ def translate():
 def translation_auto():
     input_text = request.form.get('input-text')
 
-    base_url = "https://api-free.deepl.com/v2/translate"
-    payload = {
-        "auth_key": os.environ['DEEPL_API_KEYS'],
-        "text": input_text,
-        "source_lang": "JA",
-        "target_lang": "EN"
-    }
-    
-    response = requests.get(url=base_url, params=payload)
-    
-    if response.status_code != 200:
-        raise Exception("DeepL API request failed with status code: " + str(response.status_code))
+    result = translate_by_deepl(input_text, "JA", "EN")
 
-    result = json.loads(response.text)
     return jsonify({'translation' : result['translations'][0]['text']})
 
-@app.route('/correction' , methods=['GET', 'POST'])
+@app.route('/correction-api' , methods=['POST'])
 def correction():
-    if request.method == 'GET':
-        return redirect(url_for('translate'))
-
-    input_text = request.form.get('input-text')
     translation = request.form.get('translation')
     self_translation = request.form.get('self-translation')
 
     comparison_result = json.loads(compare_sentence_meanings(translation, self_translation))
     if comparison_result["yes-or-no"] == "no":
-        comparison_result['missing-point'] = translate_by_deepl(comparison_result['missing-point'], "EN", "JA")['translations'][0]['text']
-
-    return render_template('index.html', input_text=input_text, translation=translation, self_translation=self_translation, correction=comparison_result['missing-point'])
+        comparison_result['differences'] = translate_by_deepl(comparison_result['differences'], "EN", "JA")['translations'][0]['text']
+    
+    return jsonify({'correction' : comparison_result['differences']})
 
 def compare_sentence_meanings(sentence1, sentence2):
     prompt = f"Are the meanings of the below two sentences the same? \
@@ -68,11 +52,11 @@ def compare_sentence_meanings(sentence1, sentence2):
             ### \
             Could you reply according to the following conditions? \
             ・Reply in japanese \
-            ・Follow below json format \
+            ・Reply only followin below json format \
             ### format \
             {{ \
                 \"yes-or-no\" :  answer \"yes\" or \"no\" about result of comparison two sentence. \
-                \"missing-point\" : If \"yes-or-no\" is \"no\", tell me the differences briefly in japanese. You must output empty str ('') when \"yes-or-no\" is \"yes\" \
+                \"differences\" : If \"yes-or-no\" is \"no\", tell me the differences briefly in japanese. You must output empty str ('') when \"yes-or-no\" is \"yes\" \
             }} \
             ###"
 
